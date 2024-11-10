32c4ed36cbef30923b7419d0b549e8a9a2cc37b5
Mr zakir ullah <mrzak802@gmail.com>
2024-11-10 11:50:15 -0800

    comment

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import loginIcon from "../assest/signin.gif"
+import { IoEye } from "react-icons/io5";
+import { FaEyeSlash } from "react-icons/fa";
 
 function Login() {
+    const [showPassword,setshowPassword] = useState(true)
   return (
     <section id='login'>
         <div className='mx-auto conatiner p-4'>
@@ -10,13 +13,30 @@ function Login() {
                     <img src={loginIcon} alt='login icons'/>
                 </div>
                 <form>
-                    <div>
+                    <div className='grid'>
                         <lebal>Email: </lebal>
-                        <input type='email' placeholder='Enter email'/>
+                        <div className='bg-slate-200 p-2'>
+                        <input type='email' placeholder='Enter email' className='w-full h-full outline-none bg-transparent'/>
+
+                        </div>
                     </div>
-                    <div>
+                    <div >
                         <lebal>Password: </lebal>
-                        <input type='password' placeholder='Enter password'/>
+                        <div className='bg-slate-200 p-2 flex'>
+                        <input type={showPassword? "text" : "password"} placeholder='Enter password' className='w-full h-full outline-none bg-transparent'/>
+                        <div className='cursor-pointer text-xl ' onClick={()=>setshowPassword((prevent)=>!prevent)}>
+                            <span>
+                                {
+                                    showPassword?(
+                                        <IoEye/>
+                                    ):
+                                    (
+                                        <FaEyeSlash/>
+                                    )
+                                }
+                            </span>
+                        </div>
+                        </div>
                     </div>
                     <button>Login</button>
                 </form>
