60feb43d2267ce6211e2ac9ac9dddbda58c256c0
Mr zakir ullah <mrzak802@gmail.com>
2024-11-10 06:51:37 -0800

    comment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Logo from './Logo'
+import { CiSearc } from "react-icons/ci";
+import { FaSearch } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 function Header() {
   return (
@@ -8,10 +12,23 @@ function Header() {
             <div className=''>
                 <Logo w={90} h={50}/>
             </div>
-            <div>
-              <input type='text' placeholder='search item here...'/>
+            <div className=' hidden  lg:flex  items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-3'>
+              <input type='text' placeholder='search item here...' className='w-full outline-none '/>
+              <div className='text-lg min-w-[50px] flex justify-center text-white cursor-pointer items-center rounded-r-full h-8 bg-red-500'>
+              <FaSearch />
+              </div>
             </div>
-            <div>icons user </div>
+            <div className='flex items-center gap-5'>
+              <div className='text-3xl cursor-pointer relative'>
+                <span><FaShoppingCart/></span>
+                <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-2'>
+                  <p  className='text-sm'>0</p>
+                  </div>
+              </div >
+              <div className='text-2xl'>
+              <FaUser/>
+              </div>
+            </div>
         </div>
       
 
